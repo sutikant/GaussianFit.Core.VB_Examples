8ad676b4d83b64e6b98fd05703433c40658efc76
Napat Sutikant <napat@focuz-mfg.com>
2025-11-03 17:01:13 +0700

    Commit First Draft

---
+++ TestDll/Form1.Designer.vb
@@ -0,0 +1,341 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.btnLoad = New System.Windows.Forms.Button()
+        Me.picInput = New System.Windows.Forms.PictureBox()
+        Me.picOutput = New System.Windows.Forms.PictureBox()
+        Me.rtbFit = New System.Windows.Forms.RichTextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.modeCombo = New System.Windows.Forms.ComboBox()
+        Me.percentileLabel = New System.Windows.Forms.Label()
+        Me.percentileTrackBar = New System.Windows.Forms.TrackBar()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.borderLabel = New System.Windows.Forms.Label()
+        Me.borderTrackBar = New System.Windows.Forms.TrackBar()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.btnGaussianFit = New System.Windows.Forms.Button()
+        Me.cutoffLabel = New System.Windows.Forms.Label()
+        Me.cutoffTrackBar = New System.Windows.Forms.TrackBar()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.epsStopCombo = New System.Windows.Forms.ComboBox()
+        Me.maxIterCombo = New System.Windows.Forms.ComboBox()
+        CType(Me.picInput, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picOutput, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.percentileTrackBar, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.borderTrackBar, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox2.SuspendLayout()
+        CType(Me.cutoffTrackBar, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnLoad
+        '
+        Me.btnLoad.Image = Global.TestDll.My.Resources.Resources.load_pic
+        Me.btnLoad.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btnLoad.Location = New System.Drawing.Point(10, 10)
+        Me.btnLoad.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.btnLoad.Name = "btnLoad"
+        Me.btnLoad.Size = New System.Drawing.Size(135, 51)
+        Me.btnLoad.TabIndex = 0
+        Me.btnLoad.Text = "Load Image"
+        Me.btnLoad.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.btnLoad.UseVisualStyleBackColor = True
+        '
+        'picInput
+        '
+        Me.picInput.Location = New System.Drawing.Point(149, 218)
+        Me.picInput.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.picInput.Name = "picInput"
+        Me.picInput.Size = New System.Drawing.Size(390, 280)
+        Me.picInput.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picInput.TabIndex = 1
+        Me.picInput.TabStop = False
+        '
+        'picOutput
+        '
+        Me.picOutput.Location = New System.Drawing.Point(543, 218)
+        Me.picOutput.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.picOutput.Name = "picOutput"
+        Me.picOutput.Size = New System.Drawing.Size(390, 280)
+        Me.picOutput.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picOutput.TabIndex = 2
+        Me.picOutput.TabStop = False
+        '
+        'rtbFit
+        '
+        Me.rtbFit.Location = New System.Drawing.Point(937, 12)
+        Me.rtbFit.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.rtbFit.Name = "rtbFit"
+        Me.rtbFit.Size = New System.Drawing.Size(437, 688)
+        Me.rtbFit.TabIndex = 3
+        Me.rtbFit.Text = ""
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label7)
+        Me.GroupBox1.Controls.Add(Me.modeCombo)
+        Me.GroupBox1.Controls.Add(Me.percentileLabel)
+        Me.GroupBox1.Controls.Add(Me.percentileTrackBar)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.borderLabel)
+        Me.GroupBox1.Controls.Add(Me.borderTrackBar)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(149, 11)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.GroupBox1.Size = New System.Drawing.Size(390, 202)
+        Me.GroupBox1.TabIndex = 4
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Background Substraction"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(35, 30)
+        Me.Label7.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(34, 13)
+        Me.Label7.TabIndex = 7
+        Me.Label7.Text = "Mode"
+        '
+        'modeCombo
+        '
+        Me.modeCombo.FormattingEnabled = True
+        Me.modeCombo.Location = New System.Drawing.Point(136, 28)
+        Me.modeCombo.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.modeCombo.Name = "modeCombo"
+        Me.modeCombo.Size = New System.Drawing.Size(150, 21)
+        Me.modeCombo.TabIndex = 6
+        '
+        'percentileLabel
+        '
+        Me.percentileLabel.AutoSize = True
+        Me.percentileLabel.Location = New System.Drawing.Point(314, 127)
+        Me.percentileLabel.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.percentileLabel.Name = "percentileLabel"
+        Me.percentileLabel.Size = New System.Drawing.Size(10, 13)
+        Me.percentileLabel.TabIndex = 5
+        Me.percentileLabel.Text = "-"
+        '
+        'percentileTrackBar
+        '
+        Me.percentileTrackBar.Location = New System.Drawing.Point(136, 138)
+        Me.percentileTrackBar.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.percentileTrackBar.Name = "percentileTrackBar"
+        Me.percentileTrackBar.Size = New System.Drawing.Size(211, 45)
+        Me.percentileTrackBar.TabIndex = 4
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(35, 138)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(54, 13)
+        Me.Label6.TabIndex = 3
+        Me.Label6.Text = "Percentile"
+        '
+        'borderLabel
+        '
+        Me.borderLabel.AutoSize = True
+        Me.borderLabel.Location = New System.Drawing.Point(314, 60)
+        Me.borderLabel.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.borderLabel.Name = "borderLabel"
+        Me.borderLabel.Size = New System.Drawing.Size(10, 13)
+        Me.borderLabel.TabIndex = 2
+        Me.borderLabel.Text = "-"
+        '
+        'borderTrackBar
+        '
+        Me.borderTrackBar.Location = New System.Drawing.Point(136, 72)
+        Me.borderTrackBar.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.borderTrackBar.Name = "borderTrackBar"
+        Me.borderTrackBar.Size = New System.Drawing.Size(211, 45)
+        Me.borderTrackBar.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(35, 72)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(101, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Edge Mask Size (%)"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.btnGaussianFit)
+        Me.GroupBox2.Controls.Add(Me.cutoffLabel)
+        Me.GroupBox2.Controls.Add(Me.cutoffTrackBar)
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Controls.Add(Me.Label2)
+        Me.GroupBox2.Controls.Add(Me.epsStopCombo)
+        Me.GroupBox2.Controls.Add(Me.maxIterCombo)
+        Me.GroupBox2.Location = New System.Drawing.Point(543, 12)
+        Me.GroupBox2.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Padding = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.GroupBox2.Size = New System.Drawing.Size(390, 202)
+        Me.GroupBox2.TabIndex = 5
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Gaussian Model Fitting"
+        '
+        'btnGaussianFit
+        '
+        Me.btnGaussianFit.Image = Global.TestDll.My.Resources.Resources.fit_pic
+        Me.btnGaussianFit.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btnGaussianFit.Location = New System.Drawing.Point(227, 75)
+        Me.btnGaussianFit.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.btnGaussianFit.Name = "btnGaussianFit"
+        Me.btnGaussianFit.Size = New System.Drawing.Size(136, 51)
+        Me.btnGaussianFit.TabIndex = 6
+        Me.btnGaussianFit.Text = "Gaussian Fit"
+        Me.btnGaussianFit.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.btnGaussianFit.UseVisualStyleBackColor = True
+        '
+        'cutoffLabel
+        '
+        Me.cutoffLabel.AutoSize = True
+        Me.cutoffLabel.Location = New System.Drawing.Point(276, 18)
+        Me.cutoffLabel.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.cutoffLabel.Name = "cutoffLabel"
+        Me.cutoffLabel.Size = New System.Drawing.Size(10, 13)
+        Me.cutoffLabel.TabIndex = 3
+        Me.cutoffLabel.Text = "-"
+        '
+        'cutoffTrackBar
+        '
+        Me.cutoffTrackBar.Location = New System.Drawing.Point(94, 29)
+        Me.cutoffTrackBar.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.cutoffTrackBar.Name = "cutoffTrackBar"
+        Me.cutoffTrackBar.Size = New System.Drawing.Size(211, 45)
+        Me.cutoffTrackBar.TabIndex = 2
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(4, 115)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(77, 39)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Covergence && " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Acceptance " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Criteria (ε)" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(4, 78)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(71, 13)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Max. Iteration"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(4, 29)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(93, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Cutoff Percentage"
+        '
+        'epsStopCombo
+        '
+        Me.epsStopCombo.FormattingEnabled = True
+        Me.epsStopCombo.Location = New System.Drawing.Point(94, 122)
+        Me.epsStopCombo.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.epsStopCombo.Name = "epsStopCombo"
+        Me.epsStopCombo.Size = New System.Drawing.Size(114, 21)
+        Me.epsStopCombo.TabIndex = 1
+        '
+        'maxIterCombo
+        '
+        Me.maxIterCombo.FormattingEnabled = True
+        Me.maxIterCombo.Location = New System.Drawing.Point(94, 75)
+        Me.maxIterCombo.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.maxIterCombo.Name = "maxIterCombo"
+        Me.maxIterCombo.Size = New System.Drawing.Size(114, 21)
+        Me.maxIterCombo.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1398, 720)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.rtbFit)
+        Me.Controls.Add(Me.picOutput)
+        Me.Controls.Add(Me.picInput)
+        Me.Controls.Add(Me.btnLoad)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Name = "Form1"
+        Me.Text = "Test Dll: Gaussian Fit Demo (VB)"
+        CType(Me.picInput, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picOutput, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.percentileTrackBar, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.borderTrackBar, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        CType(Me.cutoffTrackBar, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnLoad As Button
+    Friend WithEvents picInput As PictureBox
+    Friend WithEvents picOutput As PictureBox
+    Friend WithEvents rtbFit As RichTextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents epsStopCombo As ComboBox
+    Friend WithEvents maxIterCombo As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents borderLabel As Label
+    Friend WithEvents borderTrackBar As TrackBar
+    Friend WithEvents btnGaussianFit As Button
+    Friend WithEvents cutoffLabel As Label
+    Friend WithEvents cutoffTrackBar As TrackBar
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents modeCombo As ComboBox
+    Friend WithEvents percentileLabel As Label
+    Friend WithEvents percentileTrackBar As TrackBar
+    Friend WithEvents Label6 As Label
+End Class
---
+++ TestDll/Form1.vb
@@ -0,0 +1,244 @@
+﻿Imports System.Drawing
+Imports System.Drawing.Drawing2D
+Imports System.Globalization
+Imports System.IO
+Imports GaussianFit.Core
+
+Public Class Form1
+    ' === Fields (State and Cache) ===
+    ' Parameters (state)
+    Private _borderPercentage As Double = 10.0   ' 0..50 %
+    Private _cutoffPercentage As Double = 30.0   ' 1..100 %
+    Private _maxIterations As Integer = 60       ' 5..200
+    Private _epsStop As Double = 0.000000001          ' 1e-6 / 1e-9 / 1e-12
+
+    ' NEW: Mode and percentile value (for Percentile mode)
+    Private _mode As PrepMode = PrepMode.EdgeMean_BorderPct
+    Private _percentileP As Double = 10.0        ' 0.1 .. 50.0 %
+
+    Private _currentBitmap As Bitmap             ' The latest loaded image.
+
+    ' Cache (optionally used by your pipeline)
+    Private _gray8 As Byte()
+    Private _w As Integer, _h As Integer
+
+    ' After BG subtraction + threshold (optional)
+    Private _afterThreshF64 As Double()
+
+    ' === Helpers ===
+    Private Sub UpdatePreprocessUiEnableState()
+        Dim isPercentile As Boolean = (_mode = PrepMode.Percentile_GlobalP)
+
+        ' Enable p% controls only when Percentile mode
+        percentileTrackBar.Enabled = isPercentile
+        percentileLabel.Enabled = isPercentile
+
+        ' Border% is used only for EdgeMean mode
+        borderTrackBar.Enabled = Not isPercentile
+        borderLabel.Enabled = Not isPercentile
+    End Sub
+
+    ' === Form Initialization and Loading ===
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.StartPosition = FormStartPosition.CenterScreen
+
+        ' --- Border % TrackBar (0..50.0) ---
+        borderTrackBar.Minimum = 0
+        borderTrackBar.Maximum = 500           ' x10
+        borderTrackBar.TickFrequency = 50
+        borderTrackBar.Value = CInt(_borderPercentage * 10.0)
+        borderTrackBar.Width = 240
+
+        borderLabel.AutoSize = True
+        borderLabel.Text = _borderPercentage.ToString("F1", CultureInfo.InvariantCulture) & " %"
+
+        ' --- Cutoff % TrackBar (1.0..100.0) ---
+        cutoffTrackBar.Minimum = 10            ' 1.0%
+        cutoffTrackBar.Maximum = 1000          ' 100.0%
+        cutoffTrackBar.TickFrequency = 50
+        cutoffTrackBar.Value = CInt(_cutoffPercentage * 10.0)
+        cutoffTrackBar.Width = 240
+
+        cutoffLabel.AutoSize = True
+        cutoffLabel.Text = _cutoffPercentage.ToString("F1", CultureInfo.InvariantCulture) & " %"
+
+        ' --- Max Iteration Combo (5..200 step 5) ---
+        maxIterCombo.DropDownStyle = ComboBoxStyle.DropDownList
+        maxIterCombo.Items.Clear()
+        For i As Integer = 5 To 200 Step 5
+            maxIterCombo.Items.Add(i.ToString())
+        Next
+        maxIterCombo.SelectedItem = _maxIterations.ToString()
+
+        ' --- Epsilon Combo ---
+        epsStopCombo.DropDownStyle = ComboBoxStyle.DropDownList
+        epsStopCombo.Items.Clear()
+        epsStopCombo.Items.Add("1e-6")
+        epsStopCombo.Items.Add("1e-9")
+        epsStopCombo.Items.Add("1e-12")
+        epsStopCombo.SelectedItem = "1e-9"
+
+        ' === Mode Combo ===
+        modeCombo.DropDownStyle = ComboBoxStyle.DropDownList
+        modeCombo.Items.Clear()
+        modeCombo.Items.Add("Edge Mean (border %)")
+        modeCombo.Items.Add("Percentile p% (global)")
+        modeCombo.SelectedIndex = 0 ' default = EdgeMean
+
+        ' === Percentile TrackBar (0.1 .. 50.0, step 0.1) ===
+        percentileTrackBar.Minimum = 1      ' 0.1%
+        percentileTrackBar.Maximum = 500    ' 50.0%
+        percentileTrackBar.TickFrequency = 10
+        percentileTrackBar.Value = CInt(_percentileP * 10.0)
+        percentileLabel.Text = _percentileP.ToString("F1", CultureInfo.InvariantCulture) & " %"
+
+        UpdatePreprocessUiEnableState()
+
+    End Sub
+
+    ' === UI Event Handlers (Input/Configuration) ===
+    Private Sub btnLoad_Click(sender As Object, e As EventArgs) Handles btnLoad.Click
+        Using ofd As New OpenFileDialog()
+            Dim exeDir As String = AppDomain.CurrentDomain.BaseDirectory
+            Dim sampleDir As String = Path.Combine(exeDir, "sample_pic")
+            If Not Directory.Exists(sampleDir) Then Directory.CreateDirectory(sampleDir)
+
+            ofd.InitialDirectory = sampleDir
+            ofd.Filter = "Image Files|*.png;*.jpg;*.jpeg;*.bmp"
+            If ofd.ShowDialog() = DialogResult.OK Then
+                If _currentBitmap IsNot Nothing Then _currentBitmap.Dispose()
+                _currentBitmap = CType(System.Drawing.Image.FromFile(ofd.FileName), Bitmap)
+
+                If picInput.Image IsNot Nothing Then picInput.Image.Dispose()
+                picInput.Image = CType(_currentBitmap.Clone(), Bitmap)
+
+                rtbFit.Clear()
+            End If
+        End Using
+    End Sub
+
+    Private Sub borderTrackBar_Scroll(sender As Object, e As EventArgs) Handles borderTrackBar.Scroll
+        _borderPercentage = borderTrackBar.Value / 10.0
+        borderLabel.Text = _borderPercentage.ToString("F1", CultureInfo.InvariantCulture) & " %"
+    End Sub
+
+    Private Sub cutoffTrackBar_Scroll(sender As Object, e As EventArgs) Handles cutoffTrackBar.Scroll
+        _cutoffPercentage = cutoffTrackBar.Value / 10.0
+        cutoffLabel.Text = _cutoffPercentage.ToString("F1", CultureInfo.InvariantCulture) & " %"
+    End Sub
+
+    Private Sub maxIterCombo_SelectedIndexChanged(sender As Object, e As EventArgs) Handles maxIterCombo.SelectedIndexChanged
+        Dim v As Integer
+        If Integer.TryParse(Convert.ToString(maxIterCombo.SelectedItem), v) Then
+            _maxIterations = v
+        End If
+    End Sub
+
+    Private Shared Function ParseSci(s As String) As Double
+        Dim v As Double
+        If Double.TryParse(s, NumberStyles.Float, CultureInfo.InvariantCulture, v) Then
+            Return v
+        End If
+        Return 0.000000001
+    End Function
+
+    Private Sub epsStopCombo_SelectedIndexChanged(sender As Object, e As EventArgs) Handles epsStopCombo.SelectedIndexChanged
+        _epsStop = ParseSci(Convert.ToString(epsStopCombo.SelectedItem))
+    End Sub
+
+    Private Sub modeCombo_SelectedIndexChanged(sender As Object, e As EventArgs) Handles modeCombo.SelectedIndexChanged
+        _mode = If(modeCombo.SelectedIndex = 0, PrepMode.EdgeMean_BorderPct, PrepMode.Percentile_GlobalP)
+        UpdatePreprocessUiEnableState()
+    End Sub
+
+    Private Sub percentileTrackBar_Scroll(sender As Object, e As EventArgs) Handles percentileTrackBar.Scroll
+        _percentileP = percentileTrackBar.Value / 10.0 ' step 0.1
+        percentileLabel.Text = _percentileP.ToString("F1", CultureInfo.InvariantCulture) & " %"
+    End Sub
+
+    ' === Core Functionality (Gaussian Fit) ===
+    Private Sub btnGaussianFit_Click(sender As Object, e As EventArgs) Handles btnGaussianFit.Click
+        If _currentBitmap Is Nothing Then
+            MessageBox.Show("Please load an image first", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Return
+        End If
+
+        Try
+            ' Create options to match the UI settings
+            Dim opts As New FitOptions()
+            opts.Mode = _mode
+            opts.BorderPercent = _borderPercentage      ' Used for EdgeMean mode
+            opts.PercentileP = _percentileP             ' Used for Percentile mode
+            opts.CutoffPercent = _cutoffPercentage
+            opts.MaxIterations = _maxIterations
+            opts.Epsilon = _epsStop
+
+            ' Call the DLL (new API)
+            Dim fit As FitResult = GaussianFitter.Run(_currentBitmap, opts)
+
+            ' Full report
+            rtbFit.Text = fit.ReportText
+
+            ' ---- Build nRMSE line for MessageBox (like C#) ----
+            Dim nrmsePct As Double = If(Double.IsNaN(fit.NormalizedRmse), Double.NaN, fit.NormalizedRmse * 100.0)
+            Dim nrmseLine As String
+            If Double.IsNaN(nrmsePct) Then
+                nrmseLine = "nRMSE (RMSE/A):     NaN %   [Poor]"
+            Else
+                Dim pctText As String =
+                    If(nrmsePct >= 0.01,
+                       nrmsePct.ToString("F2", CultureInfo.InvariantCulture),
+                       nrmsePct.ToString("E2", CultureInfo.InvariantCulture))
+                Dim grade As String = If(String.IsNullOrEmpty(fit.QualityGrade), "—", fit.QualityGrade)
+                nrmseLine = String.Format(CultureInfo.InvariantCulture,
+                                          "nRMSE (RMSE/A): {0,10} %   [{1}]", pctText, grade)
+            End If
+
+            ' Compact summary
+            Dim msg As String = String.Format(CultureInfo.InvariantCulture,
+                "Center (x0, y0): {0:F2}, {1:F2}" & Environment.NewLine &
+                "σx, σy: {2:F3}, {3:F3}" & Environment.NewLine &
+                "FWHM X, Y: {4:F3}, {5:F3}" & Environment.NewLine &
+                "{6}",
+                fit.X0, fit.Y0, fit.SigmaX, fit.SigmaY, fit.FwhmX, fit.FwhmY, nrmseLine)
+
+            MessageBox.Show(msg, "Fit Results", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+            ' ---- Draw overlay (same as C#) ----
+            Using vis As Bitmap = CType(_currentBitmap.Clone(), Bitmap)
+                Using g As Graphics = Graphics.FromImage(vis)
+                    g.SmoothingMode = SmoothingMode.AntiAlias
+                    Dim cx As Single = CSng(fit.X0)
+                    Dim cy As Single = CSng(fit.Y0)
+
+                    ' crosshair
+                    g.DrawLine(Pens.Red, cx - 8, cy, cx + 8, cy)
+                    g.DrawLine(Pens.Red, cx, cy - 8, cx, cy + 8)
+
+                    ' helper to draw ellipse by radii
+                    Dim DrawE As Action(Of Pen, Double, Double) =
+                        Sub(pen As Pen, rx As Double, ry As Double)
+                            Dim rect As New RectangleF(CSng(cx - rx), CSng(cy - ry),
+                                                       CSng(2 * rx), CSng(2 * ry))
+                            g.DrawEllipse(pen, rect)
+                        End Sub
+
+                    Using pen1 As New Pen(Color.Green, 1),
+                          pen2 As New Pen(Color.Orange, 1),
+                          pen3 As New Pen(Color.Blue, 1)
+                        DrawE(pen1, fit.Overlay.OneSigma.Rx, fit.Overlay.OneSigma.Ry)
+                        DrawE(pen2, fit.Overlay.FwhmHalf.Rx, fit.Overlay.FwhmHalf.Ry)
+                        DrawE(pen3, fit.Overlay.OneOverE2.Rx, fit.Overlay.OneOverE2.Ry)
+                    End Using
+                End Using
+
+                If picOutput.Image IsNot Nothing Then picOutput.Image.Dispose()
+                picOutput.Image = CType(vis.Clone(), Bitmap)
+            End Using
+
+        Catch ex As Exception
+            MessageBox.Show("Gaussian Fit failed:" & Environment.NewLine & ex.Message,
+                            "Error", MessageBoxButtons.OK, MessageBoxIcon.[Error])
+        End Try
+    End Sub
+End Class
